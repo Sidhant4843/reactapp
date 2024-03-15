b630df85de59b759b89e2d379a3c068c28fcab01
Sidhant <sidhant4843.be@chitkara.edu.in>
2024-03-15 09:23:19 +0530

    spaces

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 const d =new Date();
 // System
 const month = d.getMonth();
-
 const style = {
   color: '',
   fontsize: '50px'
 };
 
+
 if(month===7||month===10||month===11) {
+
   style.color = 'red'
 }
-
 function App() {
   return (
     <div className="App">
